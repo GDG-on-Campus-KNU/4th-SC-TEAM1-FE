1eb3e4c33e386b21ea51524c4cb0bf707891cb78
KIYEONG <rldud1237@naver.com>
2025-03-25 21:06:44 +0900

    feat: 개발설정 갈아엎기 - 공부를 하자

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { Routes } from '@app/routes';
 
-import { globalStyles } from '@shared/styles';
-
-import { Global } from '@emotion/react';
-
 const App = () => {
   return (
     <>
-      <Global styles={globalStyles} />
       <Routes />
     </>
   );
