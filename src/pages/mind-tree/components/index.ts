57da0e384be875c4399b9979d85c7acace278c2e
KIYEONG <rldud1237@naver.com>
2025-05-15 01:34:54 +0900

    fix: merge 테스트 오류 해결

--- src/pages/mind-tree/components/index.ts
+++ src/pages/mind-tree/components/index.ts
@@ -1,4 +1,4 @@
 export { TreeScene } from './TreeScene';
 export { TreeStatusBar } from './TreeStatusBar';
 export { HelpModal } from './HelpModal';
-export { GuestBookModal } from './GuestbookModal';
+export { GuestBookModal } from './GuestBookModal';
